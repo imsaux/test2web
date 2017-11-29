b9ead99456539fff73d21f38a1366d49246f592e
imsuny <imsuny@home.my>
2017-11-29 08:07:11 +0800

    Initial commit

---
+++ test2web/logic.py
@@ -0,0 +1,6 @@
+import os
+import xlrd
+
+class excel_helper():
+    def __init__(self, _file):
+        self.xls = _file
---
+++ test2web/urls.py
@@ -0,0 +1,28 @@
+"""test2web URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/dev/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from test2web.page import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('page/', first_page),
+    path('time/', current_time),
+    path('show/', test),
+    path('hello2/', test2),
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
